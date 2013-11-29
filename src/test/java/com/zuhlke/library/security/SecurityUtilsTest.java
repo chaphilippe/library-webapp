47b354b8ebe90b117968ef9498769251aea0a20b
iky <iky@zuhlke.com>
2013-11-29 14:11:20 +0000

    changed admin credentials

--- src/test/java/com/zuhlke/library/security/SecurityUtilsTest.java
+++ src/test/java/com/zuhlke/library/security/SecurityUtilsTest.java
@@ -12,7 +12,9 @@ public class SecurityUtilsTest {
     public void shouldHashPassword() throws Exception {
         securityUtils.init();
         String hash = securityUtils.hash("password", "admin@zuhlke.com");
+        String adminHash = securityUtils.hash("password", "admin");
         assertEquals("7Le61jX4m+a+1TNd/ltaD7ee+jyqsgYV0Mfd0EYS18RqxBX+9YfVbkXZRd9/VdZAJ7l/SDcQn03NicBe5vnj8A==", hash);
+        assertEquals("TmE4d/EGKVXWyTWjUcCw9qoUr2bXprAzNafsV5iLHjdoPk39EAQYVxTMoacu50NlCXx70AJXsHkdN0y3mG8Ycg==", adminHash);
     }
     
 }
